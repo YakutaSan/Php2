c63f27bc0f867099d08e1e795ba6490dce7ef5bc
Alexandr Yakutin <alex.ya@protonmail.com>
2023-02-09 23:50:00 +0300

    lesson4

---
+++ lesson/src/Blog/Exceptions/JsonException.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Blog\Exceptions;
+use App\Blog\Exceptions\AppException;
+
+class JsonException extends AppException
+{
+    
+}
---
+++ lesson/src/Http/Actions/Posts/CreatePost.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Actions\Posts;
+use App\Blog\Exceptions\HttpException;
+use App\Blog\Exceptions\InvalidArgumentException;
+use App\Blog\Exceptions\UserNotFoundException;
+use App\Blog\Post;
+use App\Blog\Repositories\PostsRepository\PostsRepositoryInterface;
+use App\Blog\Repositories\UsersRepository\UsersRepositoryInterface;
+use App\Blog\UUID;
+use App\Http\Actions\ActionInterface;
+use App\Http\ErrorResponse;
+use App\Http\Request;
+use App\Http\Response;
+use App\Http\SuccessfulResponse;
+
+class CreatePost implements ActionInterface
+{
+    // Внедряем репозитории статей и пользователей
+    public function __construct(
+        private PostsRepositoryInterface $postsRepository,
+        private UsersRepositoryInterface $usersRepository,
+    )
+    {
+    }
+    public function handle(Request $request): Response
+    {
+        try {
+            $authorUuid = new UUID($request->jsonBodyField('author_uuid'));
+        } catch (HttpException | InvalidArgumentException $e) {
+            return new ErrorResponse($e->getMessage());
+        }
+
+        try {
+            $user = $this->usersRepository->get($authorUuid);
+        } catch (UserNotFoundException $e) {
+            return new ErrorResponse($e->getMessage());
+        }
+
+        $newPostUuid = UUID::random();
+
+        try {
+            $post = new Post(
+                $newPostUuid,
+                $user,
+                $request->jsonBodyField('title'),
+                $request->jsonBodyField('text'),
+            );
+        } catch (HttpException $e) {
+            return new ErrorResponse($e->getMessage());
+        }
+
+        $this->postsRepository->save($post);
+        
+        return new SuccessfulResponse([
+            'uuid' => (string) $newPostUuid,
+        ]);
+    }
+}
